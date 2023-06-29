a1d5d43ac50816de21dd15d1d6144afabfb9274e
NatorWebDev <126914912+NatorWebDev@users.noreply.github.com>
2023-06-28 23:13:14 -0400

    practicing react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Root from "./routes/root.jsx";
+import "./index.css";
+import ErrorPage from "./routes/ErrorPage.jsx";
+import Contact from "./routes/Contact.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,41 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
 ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        
+      },
+      spacing:{
+        '4':'4px',
+        '8':'8px',
+        '12':'12px',
+        '16':'16px',
+        '24':'24px',
+        '32':'32px',
+        '48':'48px',
+        '64':'64px',
+        '96':'96px',
+        '128':'128px',
+        '192':'192px',
+        '256':'256px',
+        '384':'384px',
+        '512':'512px',
+        '640':'640px',
+        '768':'768px'
+      },
+    },
+    screens: {
+      //DEFAULT desktop first
+
+      'lap': {'max' : '1024px'},
+      // => @media (min-width: 1025px) { ... }
+
+      'tab': {'max' : '768px'},
+      // => @media (max-width: 768px) { ... }
+
+      'phone':{'max' : '425px'},
+      //=>@media(max-width: 425px)
+    }
   },
   plugins: [],
 }
-
